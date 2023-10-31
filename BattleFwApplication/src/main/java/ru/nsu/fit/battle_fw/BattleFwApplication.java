7066ddcbb87468f52f568d4866c549138e1c3195
Andrew Kozubenko <a.kozubenko@g.nsu.ru>
2023-10-31 20:51:23 +0700

    App

---
+++ BattleFwApplication/src/main/java/ru/nsu/fit/battle_fw/BattleFwApplication.java
@@ -0,0 +1,13 @@
+package ru.nsu.fit.battle_fw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BattleFwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BattleFwApplication.class, args);
+	}
+
+}
