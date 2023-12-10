f31d0abb21c414ac0361067628aca7841a380f0e
Dmitry Polyachkov <d.polyachkov@g.nsu.ru>
2023-12-10 18:29:54 +0700

    del BFW

--- BattleForWeakness/src/main/resources/database/initDB.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS personasdasd
-(
-    id    INTEGER PRIMARY KEY ,
-    name  VARCHAR(200) NOT NULL ,
-    email VARCHAR(254) NOT NULL ,
-    phone VARCHAR(50)  NOT NULL
-);
-CREATE SEQUENCE clients_id_seq START WITH 1 INCREMENT BY 1;
