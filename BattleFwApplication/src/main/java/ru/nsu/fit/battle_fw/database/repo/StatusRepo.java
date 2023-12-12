76aee0dcc8c961b300e82be30fa106ae4e447c63
Dmitry Polyachkov <d.polyachkov@g.nsu.ru>
2023-12-13 01:34:36 +0700

    resolve problems with StatusRepo

--- BattleFwApplication/src/main/java/ru/nsu/fit/battle_fw/database/repo/StatusRepo.java
+++ BattleFwApplication/src/main/java/ru/nsu/fit/battle_fw/database/repo/StatusRepo.java
@@ -1,9 +1,13 @@
 package ru.nsu.fit.battle_fw.database.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import ru.nsu.fit.battle_fw.database.model.Status;
 
 @RepositoryRestResource
 public interface StatusRepo extends JpaRepository<Status, Integer> {
+    @Query("select s from Status s where s.id_game = :id_game and s.id_player = :id_player")
+    Status getStatus(@Param("id_game") Integer id_game, @Param("id_player") Integer id_player);
 }
