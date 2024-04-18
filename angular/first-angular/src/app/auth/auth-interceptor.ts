d99c7967a06127efece1c41795aa19b1fcc62e28
Andrew Kozubenko <a.kozubenko@g.nsu.ru>
2024-04-18 10:18:18 +0700

    fixed invites

--- angular/first-angular/src/app/auth/auth-interceptor.ts
+++ angular/first-angular/src/app/auth/auth-interceptor.ts
@@ -1,23 +1,46 @@
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import {HTTP_INTERCEPTORS, HttpEvent} from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http';
 
 import { TokenStorageService } from './token-storage.service';
+import {Observable} from "rxjs";
 
 const TOKEN_HEADER_KEY = 'Authorization';
+
+// @Injectable()
+// export class AuthInterceptor implements HttpInterceptor {
+//
+//   constructor(private token: TokenStorageService) { }
+//
+//   intercept(req: HttpRequest<any>, next: HttpHandler) {
+//     let authReq = req;
+//     const token = this.token.getToken();
+//     if (token != null) {
+//       authReq = req.clone({ headers: req.headers.set(TOKEN_HEADER_KEY, token) });
+//     }
+//     return next.handle(authReq);
+//   }
+// }
+//
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
-  constructor(private token: TokenStorageService) { }
+  constructor(private tokenStorageService: TokenStorageService) {}
 
-  intercept(req: HttpRequest<any>, next: HttpHandler) {
-    let authReq = req;
-    const token = this.token.getToken();
-    if (token != null) {
-      authReq = req.clone({ headers: req.headers.set(TOKEN_HEADER_KEY, token) });
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    const token = this.tokenStorageService.getToken();
+    if (token) {
+      req = req.clone({
+        setHeaders: {
+          Authorization: `Bearer ${token}`
+        }
+      });
     }
-    return next.handle(authReq);
+    return next.handle(req);
   }
 }
 
